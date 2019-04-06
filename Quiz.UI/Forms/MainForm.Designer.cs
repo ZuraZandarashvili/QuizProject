2d6b05b474d6ac9ed820281a0aa524803f0dd69c
shengo13 <mixeilshengelia@gmail.com>
2019-04-06 21:52:19 +0400

    menu

--- Quiz.UI/Forms/MainForm.Designer.cs
+++ Quiz.UI/Forms/MainForm.Designer.cs
@@ -28,12 +28,224 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.Startgamebutton = new System.Windows.Forms.Button();
+            this.Homelabel = new System.Windows.Forms.Label();
+            this.Category1label = new System.Windows.Forms.Label();
+            this.Category2label = new System.Windows.Forms.Label();
+            this.Category3label = new System.Windows.Forms.Label();
+            this.StartGamelabel = new System.Windows.Forms.Label();
+            this.Category4label4 = new System.Windows.Forms.Label();
+            this.Exitlabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderSize = 5;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Gold;
+            this.button1.Location = new System.Drawing.Point(43, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 86);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Home";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.FlatAppearance.BorderSize = 5;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.button2.Location = new System.Drawing.Point(170, 28);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(104, 86);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Home";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.FlatAppearance.BorderSize = 5;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.button3.Location = new System.Drawing.Point(292, 28);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(104, 86);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Home";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.FlatAppearance.BorderSize = 5;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.button4.Location = new System.Drawing.Point(43, 150);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(104, 86);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "Home";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.FlatAppearance.BorderSize = 5;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.button5.Location = new System.Drawing.Point(170, 150);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(104, 86);
+            this.button5.TabIndex = 0;
+            this.button5.Text = "Home";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.FlatAppearance.BorderSize = 5;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.button6.Location = new System.Drawing.Point(292, 150);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(104, 86);
+            this.button6.TabIndex = 0;
+            this.button6.Text = "Home";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // Startgamebutton
+            // 
+            this.Startgamebutton.FlatAppearance.BorderSize = 5;
+            this.Startgamebutton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Startgamebutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Startgamebutton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.Startgamebutton.Location = new System.Drawing.Point(126, 265);
+            this.Startgamebutton.Name = "Startgamebutton";
+            this.Startgamebutton.Size = new System.Drawing.Size(184, 86);
+            this.Startgamebutton.TabIndex = 0;
+            this.Startgamebutton.Text = "Start game";
+            this.Startgamebutton.UseVisualStyleBackColor = true;
+            // 
+            // Homelabel
+            // 
+            this.Homelabel.AutoSize = true;
+            this.Homelabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Homelabel.Location = new System.Drawing.Point(62, 117);
+            this.Homelabel.Name = "Homelabel";
+            this.Homelabel.Size = new System.Drawing.Size(56, 20);
+            this.Homelabel.TabIndex = 1;
+            this.Homelabel.Text = "Home";
+            // 
+            // Category1label
+            // 
+            this.Category1label.AutoSize = true;
+            this.Category1label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Category1label.Location = new System.Drawing.Point(178, 117);
+            this.Category1label.Name = "Category1label";
+            this.Category1label.Size = new System.Drawing.Size(96, 20);
+            this.Category1label.TabIndex = 1;
+            this.Category1label.Text = "Category 1";
+            // 
+            // Category2label
+            // 
+            this.Category2label.AutoSize = true;
+            this.Category2label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Category2label.Location = new System.Drawing.Point(300, 117);
+            this.Category2label.Name = "Category2label";
+            this.Category2label.Size = new System.Drawing.Size(96, 20);
+            this.Category2label.TabIndex = 1;
+            this.Category2label.Text = "Category 2";
+            // 
+            // Category3label
+            // 
+            this.Category3label.AutoSize = true;
+            this.Category3label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Category3label.Location = new System.Drawing.Point(51, 239);
+            this.Category3label.Name = "Category3label";
+            this.Category3label.Size = new System.Drawing.Size(96, 20);
+            this.Category3label.TabIndex = 1;
+            this.Category3label.Text = "Category 3";
+            // 
+            // StartGamelabel
+            // 
+            this.StartGamelabel.AutoSize = true;
+            this.StartGamelabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StartGamelabel.Location = new System.Drawing.Point(166, 354);
+            this.StartGamelabel.Name = "StartGamelabel";
+            this.StartGamelabel.Size = new System.Drawing.Size(97, 20);
+            this.StartGamelabel.TabIndex = 1;
+            this.StartGamelabel.Text = "StartGame";
+            // 
+            // Category4label4
+            // 
+            this.Category4label4.AutoSize = true;
+            this.Category4label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Category4label4.Location = new System.Drawing.Point(178, 239);
+            this.Category4label4.Name = "Category4label4";
+            this.Category4label4.Size = new System.Drawing.Size(96, 20);
+            this.Category4label4.TabIndex = 1;
+            this.Category4label4.Text = "Category 4";
+            // 
+            // Exitlabel
+            // 
+            this.Exitlabel.AutoSize = true;
+            this.Exitlabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Exitlabel.Location = new System.Drawing.Point(325, 239);
+            this.Exitlabel.Name = "Exitlabel";
+            this.Exitlabel.Size = new System.Drawing.Size(39, 20);
+            this.Exitlabel.TabIndex = 1;
+            this.Exitlabel.Text = "Exit";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(442, 378);
+            this.Controls.Add(this.Category2label);
+            this.Controls.Add(this.Category1label);
+            this.Controls.Add(this.StartGamelabel);
+            this.Controls.Add(this.Exitlabel);
+            this.Controls.Add(this.Category4label4);
+            this.Controls.Add(this.Category3label);
+            this.Controls.Add(this.Homelabel);
+            this.Controls.Add(this.Startgamebutton);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "MainForm";
             this.Text = "MainForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button Startgamebutton;
+        private System.Windows.Forms.Label Homelabel;
+        private System.Windows.Forms.Label Category1label;
+        private System.Windows.Forms.Label Category2label;
+        private System.Windows.Forms.Label Category3label;
+        private System.Windows.Forms.Label StartGamelabel;
+        private System.Windows.Forms.Label Category4label4;
+        private System.Windows.Forms.Label Exitlabel;
     }
 }
